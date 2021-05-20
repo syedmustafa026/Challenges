6bd907faffe03c362b5d78fe54caea207f6fc6be
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-05-21 00:14:28 +0500

    all commits

---
+++ app.js
@@ -0,0 +1,68 @@
+//                                        Question # 1
+
+// var user = +prompt("Enter Intial Number");
+// for (i = user; i >= 1; i--) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
+
+//                                        Question # 2
+
+// var user = +prompt("Enter Intial Number");
+// for (i = 1; i <= user; i++) {
+//     document.write( "****"+ "<br />");
+//     }
+
+//                                        Question # 3
+
+// var user = +prompt("Enter Intial Number");
+// for (i = 1; i <= user; i++) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
+
+//                                        Question # 4
+
+// var user = +prompt("Enter Intial Number");
+// for (i = user; i >= 1; i--) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
+
+//                                        Question # 5
+
+// var user = +prompt("Enter Intial Number");
+// for (i = 1; i < user; i++) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
+// for (i = user; i >= 1; i--) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
+
+//                                        Question # 6
+
+// var user = +prompt("Enter Intial Number");
+// for (i = 1; i <= user; i++) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
